741cfddda2ff16abd5e3f138f22fc97d3701712f
charity.rogers <rogerscl116@gmail.com>
2021-09-12 21:33:58 -0500

    combine gitignore files

---
+++ client/build/precache-manifest.ad29afbf9bdd0291a535343e38a9173a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b93577482af89d94b2b28d0a67b31f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f615a50790b668b2d50e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "286ab4f22ae04b2ec980",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "f615a50790b668b2d50e",
+    "url": "/static/js/2.0c2b6f1e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.0c2b6f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286ab4f22ae04b2ec980",
+    "url": "/static/js/main.7213f3c5.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
