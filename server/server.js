b73d7381939f53c7ffab5745d84d71215ab2cf0a
charity.rogers <rogerscl116@gmail.com>
2021-09-12 19:50:33 -0500

    npm install

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
 
 // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
